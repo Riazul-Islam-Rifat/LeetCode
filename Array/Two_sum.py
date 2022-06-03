c35b5474d6a4b7db8c9b1dc5bdaa2e97cb6df556
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-06-03 23:23:06 +0600

    Update Two_sum.py

--- Array/Two_sum.py
+++ Array/Two_sum.py
@@ -1,4 +1,22 @@
-given_arr=input()
+# With time complexity o(n)
+def twoSum(arr,target):
+    tracker={}
+    for index, value in enumerate(arr):
+        difference=target-value
+        if difference in tracker:
+            return [tracker[difference],index]
+        tracker[value]=index
+
+inpt = input()
+target= int(input())
+array=list(map(int,inpt.split()))
+result=twoSum(array,target)
+print(result)
+
+
+# With o(n^2) time complexity.
+
+'''given_arr=input()
 modified_arr=str(given_arr[1:-1])
 array=list(map(int, modified_arr.split(',')))
 target_num=int(input())
@@ -10,4 +28,4 @@ for item in range(0,len(array)):
             #final_arr.extend([item,i])
             final_arr="["+str(item)+','+str(i)+']'
             break
-print(final_arr)
+print(final_arr)'''
