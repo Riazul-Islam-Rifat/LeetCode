591ff724620004b701e4f54ba7078bbaf6178b30
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-06-22 20:01:16 +0600

    Update Root equal sum of children.py

---
+++ Tree/Root equal sum of children.py
@@ -0,0 +1,40 @@
+class BinaryTree:
+
+    def __init__(self,data):
+        self.root=data
+        self.left=None
+        self.right=None
+
+    # Inserting value to the left and right of the root
+    def insert(self,value):
+        global arr
+        if self.root:
+            if value<self.root:
+                if self.left is None:
+                    self.left=BinaryTree(value)
+                    arr.append(self.left.root)
+                else:
+                    self.left.insert(value)
+
+            elif value>self.root:
+                if self.right is None:
+                    self.right=BinaryTree(value)
+                    arr.append(self.right.root)
+                else:
+                    self.right.insert(value)
+        else:
+            self.root=value
+            arr.append(self.root)
+    @staticmethod
+    def root_sum():
+        if arr[0]==arr[1]+arr[2]:
+            print("True")
+        else:
+            print("False")
+
+arr=[]
+tree=BinaryTree(5)
+arr.append(tree.root)
+tree.insert(3)
+tree.insert(1)
+tree.root_sum()
