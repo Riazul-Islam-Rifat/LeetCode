72482856c10ffccba078c73624d642038d89ac84
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-07-05 21:24:27 +0600

    Update Node of a binary tree in a clone of that tree.py

--- Tree/Node of a binary tree in a clone of that tree.py
+++ Tree/Node of a binary tree in a clone of that tree.py
@@ -1,11 +1,11 @@
 # Leetcode solution
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
