2f78e2e6732a5ee60145a90cd3f159298c63dc66
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-07-22 23:49:21 +0600

    Update Create a binary tree.py

--- Tree/Create a binary tree.py
+++ Tree/Create a binary tree.py
@@ -29,15 +29,41 @@ class BinaryTree:
         if self.right:
             self.right.in_order()
 
+    # For finding paths
+    def pathPrint(self):
+        path=''
+        path_list=[]
+        def all_path(root,path):
+            if root is None:
+                return ()
 
-root=BinaryTree(10)
-root.insert(2)
-root.insert(4)
-root.insert(6)
-root.insert(8)
-root.insert(11)
-root.insert(13)
-root.insert(15)
-root.insert(17)
+            if root.left is None and root.right is None:
+                path = path + str(root.root) # Here first root is the object and 2nd root is the object's value
+                print()
+                print("After traversing a full path-- ",path)
+                path_list.append(int(path))
+                print("Path List", path_list)
+
+            all_path(root.left, path + str(root.root)) # Here first root is the object and 2nd root is the object's value
+            all_path(root.right, path + str(root.root)) # Here first root is the object and 2nd root is the object's value
+
+
+        all_path(self,path)
+        return sum(path_list)
+        #return all_path(root, path)
+
+root=BinaryTree("6")
+root.insert("8")
+root.insert("7")
+root.insert("9")
+root.insert("3")
+root.insert("2")
+root.insert("4")
+
+# In order print
 root.in_order()
+
+# For path
+result = root.pathPrint()
+print('sum_of_all_path', result)
 
