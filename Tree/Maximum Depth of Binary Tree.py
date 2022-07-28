adba9cc6ca14fb7341c293c809eb2ad5e123d074
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-07-28 23:06:02 +0600

    Update Maximum Depth of Binary Tree.py

--- Tree/Maximum Depth of Binary Tree.py
+++ Tree/Maximum Depth of Binary Tree.py
@@ -1,44 +1,11 @@
-class BinaryTree:
-    def __init__(self,value):
-        self.root=value
-        self.left=None
-        self.right=None
-
-    def insert(self,value):
-        if self.root:
-            if value>self.root:
-                if self.right:
-                    self.right.insert(value)
-                else:
-                    self.right=BinaryTree(value)
-            else:
-                if self.left:
-                    self.left.insert(value)
-                else:
-                    self.left=BinaryTree(value)
-        else:
-            self.root=value
-
-
-    def maxDepth(self):
-
-        if not self:
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root) -> int:
+        if not root:
             return 0
-        if self.left and self.right:
-            return max(self.left.maxDepth(),self.right.maxDepth())+1
-        # if self.left:
-        #     left = self.left.maxDepth()
-        #
-        # if self.right:
-        #     right = self.right.maxDepth()
-        # return max(left,right) + 1
-
-root=BinaryTree("6")
-root.insert("8")
-root.insert("7")
-root.insert("9")
-root.insert("3")
-
-result= root.maxDepth()
-
-print(result)
+        return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
