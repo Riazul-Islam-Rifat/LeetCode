0cb78344b7009b7ca9809f86c42c0279d4e34931
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-08-30 23:36:59 +0600

    Update 2181. Merge Nodes in Between Zeros.py

--- Linked List/2181. Merge Nodes in Between Zeros.py
+++ Linked List/2181. Merge Nodes in Between Zeros.py
@@ -6,11 +6,11 @@ class ListNode:
 class Solution:
     def mergeNodes(self, head: [ListNode]) -> [ListNode]:
 
-        current = head  # currenr and head shares the same address
+        current = head  # current and head shares the same address
 
         # The following while condition stisfies when there are at least one value in between two 0s. [0,1,0]/[0,1,2,0,3,0]
         while current and current.next.next:
-            # Add the non zero value and update the next
+            # Add the non-zero value and update the next
             if current.next.val != 0:
                 current.val += current.next.val
                 current.next = current.next.next  # for [0,1,0] current.next indicates the last 0.
