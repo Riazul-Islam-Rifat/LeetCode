eca08098006eed2c274aa7a8a2f4ffcea3e72ce0
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-10-30 00:05:50 +0600

    Create 2453. Destroy Sequential Targets.py

---
+++ Array/2453. Destroy Sequential Targets.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def destroyTargets(self, nums: List[int], space: int) -> int:
+        groups = defaultdict(list)
+        for num in nums:
+            groups[num % space].append(num)
+
+        performance = defaultdict(list)
+        for group in groups.values():
+            performance[len(group)].append(min(group))
+
+        return min(performance[max(performance)])
