25c6b92c9cf107cdbe5b5c4d57c0a1dbeffe518e
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2022-11-01 23:59:26 +0600

    Create 1859. Sorting the Sentence.py

---
+++ String/1859. Sorting the Sentence.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+
+        str_list = s.split(' ')
+        tracker = [''] * len(str_list)
+
+        for item in str_list:
+            tracker[int(item[-1]) - 1] = item[:-1]
+
+        output_str = ''
+
+        for item in range(len(tracker)):
+
+            if item == len(tracker) - 1:
+                output_str += tracker[item]
+
+            else:
+                output_str += tracker[item] + ' '
+
+        return output_str
+
+
+
