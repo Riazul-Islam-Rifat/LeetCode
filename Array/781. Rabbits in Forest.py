d9062c6ee0d92b4dc1b789e8374d8ff6411248d3
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2023-01-07 20:46:08 +0600

    Create 781. Rabbits in Forest.py

---
+++ Array/781. Rabbits in Forest.py
@@ -0,0 +1,11 @@
+from collections import Counter
+import math
+
+
+class Solution:
+    def numRabbits(self, answers: [int]) -> int:
+        tracker = Counter(answers)
+        rabbits = 0
+        for key, value in tracker.items():
+            rabbits += ((key + 1) * (math.ceil(value / key + 1)))
+        return rabbits
