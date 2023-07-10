8a3bd782f9c75235b51d6e8d62a24d086d5fd141
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2023-07-11 00:13:25 +0600

    Create 2545. Sort the Students by Their Kth Score.py

---
+++ Array/2545. Sort the Students by Their Kth Score.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        score.sort(reverse=True, key=lambda x:x[k])
+        return score
