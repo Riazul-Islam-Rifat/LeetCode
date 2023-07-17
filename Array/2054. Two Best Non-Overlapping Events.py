e3a368e1e61bc6bcf46c18733c6218e2078ca3d8
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2023-07-17 20:23:09 +0600

    Create 2054. Two Best Non-Overlapping Events.py

---
+++ Array/2054. Two Best Non-Overlapping Events.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        tracker = []
+        res = 0
+        CurrentMaxVal = 0  # Max value of finished events so far
+
+        for start, end, val in events:
+            tracker.append([start, True, val])  # Starting time, started, value
+            tracker.append([end + 1, False, val])  # End time (inclusive), ended, value
+
+        print(tracker)
+
+        tracker.sort()  # Sort according to starting time
+
+        print(tracker)
+
+        for time, is_start, val in tracker:
+            if is_start:
+                res = max(res, CurrentMaxVal + val)
+            else:
+                CurrentMaxVal = max(CurrentMaxVal, val)
+        return res
