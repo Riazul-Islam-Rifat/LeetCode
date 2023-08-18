2428001820c555c567c2af5c3f1fca24431135d8
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2023-08-18 22:12:04 +0600

    Create 42. Trapping Rain Water.py

---
+++ MUST 150/42. Trapping Rain Water.py
@@ -0,0 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        leftIdx = 0
+        rightIdx = len(height) - 1
+        leftMax = height[leftIdx]
+        rightMax = height[rightIdx]
+        water = 0
+        while leftIdx < rightIdx:
+            if height[leftIdx] < height[rightIdx]:
+                leftIdx += 1
+                leftMax = max(leftMax, height[leftIdx])
+                water += leftMax - height[leftIdx]
+
+            else:
+                rightIdx -= 1
+                rightMax = max(rightMax, height[rightIdx])
+                water += rightMax - height[rightIdx]
+
+        return water 
