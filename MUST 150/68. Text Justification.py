c4cf6a5bf3156068117e7f20c2b0a7302ab8d00f
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2023-08-25 17:26:22 +0600

    Create 68. Text Justification.py

---
+++ MUST 150/68. Text Justification.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fullJustify(self, words: [str], maxWidth: int) -> [str]:
+        res, line, width = [], [], 0
+
+        for w in words:
+            if width + len(w) + len(line) > maxWidth:
+                for i in range(maxWidth - width):
+                    line[i % max(len(line) - 1 , 1)] += ' '
+                res, line, width = res + [''.join(line)], [], 0
+            line += [w]
+            width += len(w)
+
+        return res + [' '.join(line).ljust(maxWidth)]
