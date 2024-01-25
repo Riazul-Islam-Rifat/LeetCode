d1ce38871d8a4d7476815b1bd64b56b4e94d6e7f
Riazul-Islam-Rifat <riazulislamrifat928@gmail.com>
2024-01-25 14:38:10 +0600

    Create 3005. Count Elements With Maximum Frequency.py

---
+++ Array/3005. Count Elements With Maximum Frequency.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxFrequencyElements(self, nums: [int]) -> int:
+        freq = {}
+
+        for item in nums:
+            if item in freq:
+                freq[item] += 1
+
+            else:
+                freq[item] = 1
+
+        maxFreq = sorted(freq.items(), key=lambda item: item[1], reverse=True)
+
+        freqs = []
+
+        for i in maxFreq:
+            freqs.append(i[1])
+
+        maxVal = freqs.count(freqs[0])
+
+        return maxVal * freqs[0]
