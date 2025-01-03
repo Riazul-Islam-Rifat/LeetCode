5cea7f31f8d9ef34dcb771cedbf59e20113db2fb
Riazul Islam Rifat <riazulislamrifat928@gmail.com>
2025-01-03 13:13:03 +0600

    Create 2750. Ways to Split Array Into Good Subarrays.py

---
+++ Array/2750. Ways to Split Array Into Good Subarrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfGoodSubarraySplits(self, nums: List[int]) -> int:
+        cnt = 0
+        lo = -1
+        for hi, b in enumerate(nums):
+            if b == 1:
+                if cnt == 0:
+                    cnt = 1
+                else:
+                    cnt *= hi - lo
+                    # print(cnt)
+                    cnt %= 10 ** 9 + 7
+                lo = hi
+        return cnt
